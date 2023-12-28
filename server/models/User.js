c514d88a46dac7c102539332f0e11e3f5319ac8e
Elvis Chen <elvischen291@gmail.com>
2023-12-28 00:50:50 -0600

    init

--- server/models/User.js
+++ server/models/User.js
@@ -1,15 +1,16 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
-    {
-        username:{type: String, required: true, unique: true},
-        email:{type: String, requirde: true, unique: true},
-        password:{type: String, required: true},
-        location:{type: String, required: false},
-        isCertified:{type: Boolean, default: false},//Certified user can have certification label
-        isAgent:{type: Boolean, default: false},//Agent user can upload their studios for rent
-        profile:{type: String, required: false},
-    },  {timestamps: true}
-)
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    location: { type: String, required: false },
+    isCertified: { type: Boolean, default: false }, //Certified user can have certification label
+    isAgent: { type: Boolean, default: false }, //Agent user can upload their studios for rent
+    profile: { type: String, required: false },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("User", UserSchema)
+module.exports = mongoose.model("User", UserSchema);
