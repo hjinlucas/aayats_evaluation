8ae7cfb72bc99e7958418fae97e46b8327664097
ryanlavu <ryanvu@ucsb.edu>
2024-01-08 14:07:08 -0800

    done

---
+++ lib/view/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'beats/beats.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(393, 852),
+      child: MaterialApp(
+        color: Colors.black,
+        debugShowCheckedModeBanner: false,
+        home: Beats(),
+      ),
+    );
+  }
+}
